0f04e83467dcd054eefda6c6dc6ac429bb5c7791
Nurtore Kambar <nurtorekambar21@gmail.com>
2026-08-25 12:32:54 +0500

    Initial Commit

---
+++ Custom tab bar with action button/Custom_tab_bar_with_action_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_tab_bar_with_action_buttonApp.swift
+//  Custom tab bar with action button
+//
+//  Created by Nurtore on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct Custom_tab_bar_with_action_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
